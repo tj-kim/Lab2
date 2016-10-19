1303e8cffbeba297ae3e782bb0111eb8aac7cb1a
tj-kim <taejin.kim96@gmail.com>
2016-10-18 20:34:15 -0400

    fixed shift register

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -38,29 +38,41 @@ module testshiftregister();
         clk=1; 
         
         peripheralClkEdge=0;
-        parallelLoad=0; parallelDataIn=8'b00000000; serialDataIn=1;#10
-        $display("  %b  |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000000   0",  1, 1'b0, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        parallelLoad=0; parallelDataIn=8'b00010000; serialDataIn=1;#10
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000000   0",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
         
-        parallelLoad=1;#10
-        $display("  %b  |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000000   0",  1, 1'b0, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut); 
+        parallelLoad=0; parallelDataIn=8'b00010000; serialDataIn=1;#1
+        peripheralClkEdge=1;#2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000000   0",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 0; #1
+
+        parallelLoad=1;#2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000001   0",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+
+        parallelLoad = 0; #2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000000   0",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut); 
 
         parallelLoad=0; serialDataIn=1;#8
-        peripheralClkEdge = 1; #10
-        $display("  %b  |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000001   0",  1, 1'b1, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 1; #2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  00000001   0",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 0; #1
         
-        peripheralClkEdge = 0; #10
         parallelLoad=0; parallelDataIn=8'b11110000; serialDataIn=1; #10 
-        peripheralClkEdge = 1; #10
-        $display("  %b  |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  11100001   1",  1, 1'b0, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 1; #2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  11100001   1",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge =0; #1
         
         parallelLoad=1; parallelDataIn=8'b11110000; serialDataIn=1; #10 
-        $display("  %b  |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  11100001   1",  1, 1'b0, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 1; #2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  11100001   1",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
         
-        parallelLoad=1; parallelDataIn=8'b11110000; serialDataIn=1;
-        $display("  %b  |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  11100001   1",  1, 1'b1, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 0; #1
+        parallelLoad=1; parallelDataIn=8'b11110000; serialDataIn=1; #2
+        peripheralClkEdge = 1; #2
+        $display("  %b   |    %b     |    %b   |   %b   |   %b   |   %b    |   %b    |  11100001   1",  clk, peripheralClkEdge, parallelLoad, parallelDataIn, serialDataIn, parallelDataOut, serialDataOut);
+        peripheralClkEdge = 0; #1
 
-
-        #60 $finish;
+        #10 $finish;
     end
 
 endmodule
--- shiftregister.v
+++ shiftregister.v
@@ -24,7 +24,7 @@ output              serialDataOut       // Positive edge synchronized
         if (parallelLoad) begin
         	shiftregistermem <= parallelDataIn;
         end
-        else @(posedge peripheralClkEdge) begin
+        else if (peripheralClkEdge) begin
         	shiftregistermem <= shiftregistermem << 1;
         	shiftregistermem[0] <= serialDataIn;
         end
@@ -33,3 +33,46 @@ output              serialDataOut       // Positive edge synchronized
     assign serialDataOut = shiftregistermem[width-1];
 
 endmodule
+
+
+// module shiftregister // done w mux
+// #(parameter width = 8)
+// (
+// input               clk,                // FPGA Clock
+// input               peripheralClkEdge,  // Edge indicator
+// input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
+// input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
+// input               serialDataIn,       // Load shift reg serially
+// output [width-1:0]  parallelDataOut,    // Shift reg data contents
+// output              serialDataOut       // Positive edge synchronized
+// );
+
+//     reg [width-1:0]      shiftregistermem;
+//     reg [width-1:0]      shiftregistermem1;
+//     reg [width-1:0]      shiftregistermem2;
+//     reg [width-1:0]      for_i;
+//     always @(posedge clk) begin
+//         // Your Code Here
+//         shiftregistermem2 <= parallelDataIn;
+
+//         if (peripheralClkEdge) begin
+//             shiftregistermem1 <= shiftregistermem <<1;
+//             shiftregistermem1[0] <= serialDataIn;
+//             // shiftregistermem1 = {}
+//         end
+
+
+//         if (parallelLoad) begin
+//             shiftregistermem <= shiftregistermem2;
+//         end
+//         else begin
+//             shiftregistermem <= shiftregistermem1;
+//         end
+
+
+
+//     end
+//     assign parallelDataOut = shiftregistermem;
+//     assign serialDataOut = shiftregistermem[width-1];
+
+// endmodule
